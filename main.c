6b78e648f5accbeb9b281c93e1d5c1202ab6996c
Michael <michaelbrockus@gmail.com>
2021-03-22 09:50:12 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+// file: main.c
+// author: Michael Brockus
+// gmail: <michaelbrockus@gmail.com>
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+// main is where program execution starts
+int main(void)
+{
+    printf("%s", "Hello, Coding World!");
+    return EXIT_SUCCESS;
+} // end of function main
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Code Notes for C programmers
+
+## About
+
+* * *
+
+The `Code Note` project is a educational project that consists of code samples and written
+information for your learning experience. Feel free to compile and run any of the samples
+you like.
+
+## Content
+
+TODO
