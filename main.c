0b1a712ccfee5d9cb60989c3361fe89314397ced
Michael Brockus <55331536+michaelbrockus@users.noreply.github.com>
2021-03-22 10:07:13 -0700

    write sample prog

--- main.c
+++ main.c
@@ -9,6 +9,18 @@
 // main is where program execution starts
 int main(void)
 {
-    printf("%s", "Hello, Coding World!");
+    int integerOne; // first integer to be entered by user
+    int integerTwo; // second integer to be entered by user
+
+    puts("Enter first integer"); // prompt the user
+    scanf("%d", & integerOne); // read value into integer one after it is read
+
+    puts("Enter second integer"); // prompt the user one more time
+    scanf("%d", & integerTwo); // read value into integer two after it is read
+
+    int summary; // variable in which the sum is stored
+    summary = integerOne + integerTwo; // assing total to be the sum
+
+    printf("%s %d", "Sum is", summary); // output the summary
     return EXIT_SUCCESS;
 } // end of function main
